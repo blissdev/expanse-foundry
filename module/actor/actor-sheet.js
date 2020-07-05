c017137550594aaf174efeb3e232e68193eb3918
Jordan Arentsen <blissdev@gmail.com>
2020-07-05 01:49:36 -0500

    Initial setup for ability roll calculations
    Trying to figure out how to send custom messages.

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -114,6 +114,7 @@ export class BoilerplateActorSheet extends ActorSheet {
 
     // Rollable abilities.
     html.find('.rollable').click(this._onRoll.bind(this));
+    html.find('.ability-rollable').click(this._onAbilityRoll.bind(this));
 
     // Drag events for macros.
     if (this.actor.owner) {
@@ -167,6 +168,44 @@ export class BoilerplateActorSheet extends ActorSheet {
       let roll = new Roll(dataset.roll, this.actor.data.data);
       let label = dataset.label ? `Rolling ${dataset.label}` : '';
       roll.roll().toMessage({
+        speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+        flavor: label
+      });
+    }
+  }
+
+  /**
+   * Handle clickable rolls.
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  _onAbilityRoll(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const dataset = element.dataset;
+
+    if (dataset.roll) {
+      let roll = new Roll("{2d6, 1d6}", this.actor.data.data);
+      let label = dataset.label ? `Rolling ${dataset.label}` : '';
+      let rollResult = roll.roll();
+
+      let normalResults = rollResult.dice[0].rolls.map(r => r.roll);
+      let dramaResult = rollResult.dice[1].rolls.map(r => r.roll);
+      let combinedResults = [...normalResults, ...dramaResult];
+      let total = rollResult.total;
+
+      let double = new Set(combinedResults).size < combinedResults.length;
+
+      let stunt = double ? dramaResult[0] : 0;
+      
+      console.log({
+        combinedResults,
+        dramaResult,
+        double,
+        stunt,
+      });
+
+      rollResult.toMessage({
         speaker: ChatMessage.getSpeaker({ actor: this.actor }),
         flavor: label
       });
